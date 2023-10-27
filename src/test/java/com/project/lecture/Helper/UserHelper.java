588085fc39d0f997a2c1547ba2db90a5a1a3bf56
dkwktm45 <dkwktm45@naver.com>
2023-10-27 18:05:28 +0900

    test : [강좌 create api]

--- src/test/java/com/project/lecture/Helper/UserHelper.java
+++ src/test/java/com/project/lecture/Helper/UserHelper.java
@@ -1,9 +1,16 @@
 package com.project.lecture.Helper;
 
+import com.project.lecture.course.dto.CourseRequest;
+import com.project.lecture.course.dto.CourseRequest.Create;
+import com.project.lecture.course.dto.LectureDto;
+import com.project.lecture.entity.Course;
+import com.project.lecture.entity.Lecture;
 import com.project.lecture.entity.Member;
 import com.project.lecture.type.AuthType;
 import com.project.lecture.type.SocialType;
 import com.project.lecture.user.dto.UserRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserHelper {
   public static UserRequest.SignUp createSignUpForm(){
@@ -24,5 +31,32 @@ public class UserHelper {
         .socialType(SocialType.PLANNER)
         .socialId("1234")
         .authType(AuthType.USER.getDescription()).build();
+  }
+
+  public static Course createCourseForm() {
+    List<Lecture> list = new ArrayList<>();
+
+    for (int i = 1; i <= 3; i++) {
+      list.add(Lecture.builder()
+          .lectureTime(i)
+          .lectureName(i + "번째 강의").build());
+    }
+
+    return Course.builder()
+        .lectures(list)
+        .courseName("제로베이스")
+        .courseContent("제로베이스 백앤드").build();
+  }
+  public static Create createCourseCreateForm() {
+    List<LectureDto> list = new ArrayList<>();
+
+    for (int i = 1; i <= 3; i++) {
+      list.add(new LectureDto(i + "name", i));
+    }
+
+    return CourseRequest.Create.builder()
+        .lectures(list)
+        .courseName("제로베이스")
+        .courseContent("제로베이스 백앤드").build();
   }
 }
---
+++ src/test/java/com/project/lecture/course/service/CourseServiceTest.java
@@ -0,0 +1,47 @@
+package com.project.lecture.course.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.project.lecture.Helper.UserHelper;
+import com.project.lecture.course.dto.CourseRequest;
+import com.project.lecture.entity.Course;
+import com.project.lecture.repository.CourseRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CourseServiceTest {
+
+  @Mock
+  private CourseRepository courseRepository;
+  @InjectMocks
+  private CourseService courseService;
+
+  @Test
+  @DisplayName("강좌 생성")
+  void createCourse() {
+    CourseRequest.Create arg = UserHelper.createCourseCreateForm();
+    Course course = UserHelper.createCourseForm();
+
+    when(courseRepository.save(any()))
+        .thenReturn(course);
+
+    Course result = courseService.createCourse(arg);
+
+    assertEquals(result.getCourseContent(),course.getCourseContent());
+    assertEquals(result.getCourseName(),course.getCourseName());
+    for (int i = 0; i < course.getLectures().size(); i++) {
+      assertEquals(course.getLectures().get(i)
+          ,result.getLectures().get(i));
+    }
+    verify(courseRepository, timeout(1)).save(any());
+  }
+}
