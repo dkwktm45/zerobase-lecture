aee751339d435a2b27ca836f85a321531ac2e6da
dkwktm45 <dkwktm45@naver.com>
2023-11-03 11:46:49 +0900

    feat : [회고(Reflection) entity 생성]

---
+++ src/main/java/com/project/lecture/repository/ReflectionRepository.java
@@ -0,0 +1,8 @@
+package com.project.lecture.repository;
+
+import com.project.lecture.entity.Reflection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReflectionRepository extends JpaRepository<Reflection,Long> {
+
+}
